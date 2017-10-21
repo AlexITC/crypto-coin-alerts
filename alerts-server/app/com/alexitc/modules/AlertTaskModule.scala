4ac1da1188c7a871bd8517cfa46e927e58b19b97
Alexis Hernandez <alexis22229@gmail.com>
2017-10-21 18:50:17 -0500

    alerts-server: Schedule to run the alert task

---
+++ alerts-server/app/com/alexitc/modules/AlertTaskModule.scala
@@ -0,0 +1,38 @@
+package com.alexitc.modules
+
+import javax.inject.Inject
+
+import akka.actor.ActorSystem
+import org.slf4j.LoggerFactory
+import play.api.inject.{SimpleModule, _}
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+class AlertTaskModule
+    extends SimpleModule(bind[AlertTaskRunner].toSelf.eagerly())
+
+/**
+ * TODO: Use custom execution context
+ *
+ * @param actorSystem
+ * @param executionContext
+ */
+class AlertTaskRunner @Inject() (
+    actorSystem: ActorSystem)(
+    implicit executionContext: ExecutionContext) {
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  logger.info("Starting alert task runner...")
+
+  // TODO: Read values from config
+  actorSystem.scheduler.schedule(
+    initialDelay = 1.minute,
+    interval = 5.minutes,
+  )(runTask)
+
+  def runTask = {
+    logger.info("Delivering alerts...")
+  }
+}
