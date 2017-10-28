93081257839d18f67b711d6a4c2c6e3f247692cf
Alexis Hernandez <alexis22229@gmail.com>
2017-10-27 21:24:45 -0500

    alerts-server: Ensure play evolutions are applyed when the app starts

--- alerts-server/conf/evolutions/default/1.sql
+++ alerts-server/conf/evolutions/default/1.sql
@@ -9,13 +9,29 @@ CREATE TABLE users(
   email CITEXT NOT NULL,
   password VARCHAR(64) NOT NULL,
   created_on TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  verified_on TIMESTAMP WITH TIME ZONE NULL,
   -- constraints
   CONSTRAINT users_user_id_pk PRIMARY KEY (user_id),
   CONSTRAINT users_email_is_unique UNIQUE (email),
   CONSTRAINT users_email_is_not_empty CHECK (email <> ''),
-  CONSTRAINT users_password_is_not_empty CHECK (password <> '')
+  CONSTRAINT users_password_is_not_empty CHECK (password <> ''),
+  CONSTRAINT verified_on_is_after_created_on CHECK (verified_on > created_on)
 );
+CREATE INDEX users_email_index ON users USING BTREE (email);
 CREATE INDEX users_created_on_index ON users USING BTREE (created_on);
+
+
+CREATE TABLE user_verification_tokens(
+  user_id VARCHAR(40) NOT NULL,
+  token VARCHAR(150) NOT NULL,
+  requested_on TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  -- constraints
+  CONSTRAINT user_verification_tokens_user_id_pk PRIMARY KEY (user_id),
+  CONSTRAINT user_verification_tokens_user_id_fk FOREIGN KEY (user_id) REFERENCES users (user_id),
+  CONSTRAINT user_verification_tokens_token_is_not_empty CHECK (token <> '')
+);
+CREATE INDEX user_verification_tokens_token_index ON user_verification_tokens USING BTREE (TOKEN);
+
 
 -- Represents a market like BITTREX, POLONIEX, BITSO
 CREATE TABLE markets(
@@ -65,4 +81,5 @@ CREATE INDEX alerts_price_index ON alerts USING BTREE (price); -- trigger alert 
 DROP TABLE base_price_alerts;
 DROP TABLE alerts;
 DROP TABLE markets;
+DROP TABLE user_verification_tokens;
 DROP TABLE users;
