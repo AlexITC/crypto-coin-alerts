6109f6b8f33d46ed44a89c78476d5013f7476604
Alexis Hernandez <alexis22229@gmail.com>
2017-10-29 17:38:30 -0600

    alerts-server: Add MessageKey model

---
+++ alerts-server/app/com/alexitc/coinalerts/models/MessageKey.scala
@@ -0,0 +1,11 @@
+package com.alexitc.coinalerts.models
+
+/**
+ * Represents a message key to use with play i18n.
+ *
+ * The use of this model allow us to differentiate
+ * the messages that are not adequate to display.
+ *
+ * @param string
+ */
+case class MessageKey(string: String) extends AnyVal
