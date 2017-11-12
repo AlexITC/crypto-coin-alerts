f27d5c53ce83c0075b7ba8f302d650a4f5618979
Alexis Hernandez <alexis22229@gmail.com>
2017-11-12 00:08:51 -0600

    alerts-server: Add anorm parsers for alert related models

--- alerts-server/app/com/alexitc/coinalerts/data/anorm/AnormParsers.scala
+++ alerts-server/app/com/alexitc/coinalerts/data/anorm/AnormParsers.scala
@@ -11,8 +11,25 @@ object AnormParsers {
   val parseEmail = str("email")(citextToString).map(UserEmail.apply)
   val parseUserVerificationToken = str("token").map(UserVerificationToken.apply)
 
+  val parseAlertId = long("alert_id").map(AlertId.apply)
+  val parseAlertType = str("alert_type").map(AlertType.fromDatabaseString)
+  val parseMarket = str("market").map(Market.fromDatabaseString)
+  val parseBook = str("book").map(Book.fromString(_).get) // Assumes db value is always properly formatted.
+  val parseisGreaterThan = bool("is_greater_than")
+  val parsePrice = get[BigDecimal]("price")
+  val parseBasePrice = get[BigDecimal]("base_price")
+
   val parseUser = (parseUserId ~ parseEmail).map {
     case userId ~ email => User.apply(userId, email)
+  }
+
+  val parseAlert = (parseAlertId ~ parseAlertType ~ parseUserId ~ parseMarket ~ parseBook ~ parseisGreaterThan ~ parsePrice).map {
+    case alertId ~ alertType ~ userId ~ market ~ book ~ isGreaterThan ~ price =>
+      Alert(alertId, alertType, userId, market, book, isGreaterThan, price)
+  }
+
+  val parseBasePriceAlert = (parseAlertId ~ parseBasePrice).map {
+    case alertId ~ basePrice => BasePriceAlert(alertId, basePrice)
   }
 
   val parsePassword = str("password").map(UserHiddenPassword.fromDatabase)
