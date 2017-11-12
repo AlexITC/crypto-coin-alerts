f3bfd9da3bed572db788908b19bf766791d8fc6e
Alexis Hernandez <alexis22229@gmail.com>
2017-11-12 00:05:10 -0600

    alerts-server: Add Book model

---
+++ alerts-server/app/com/alexitc/coinalerts/models/Book.scala
@@ -0,0 +1,27 @@
+package com.alexitc.coinalerts.models
+
+import play.api.libs.json._
+
+case class Book(major: String, minor: String) {
+  val string = s"${major}_$minor"
+}
+object Book {
+
+  def fromString(string: String): Option[Book] = {
+    Option(string.split("_"))
+        .filter(_.length == 2)
+        .map { parts =>
+          val major = parts(0)
+          val minor = parts(1)
+          Book(major, minor)
+        }
+  }
+
+  implicit val reads: Reads[Book] = {
+    JsPath.read[String].collect(JsonValidationError("error.book.invalid")) {
+      case string if fromString(string).isDefined => fromString(string).get
+    }
+  }
+
+  implicit val writes: Writes[Book] = Writes[Book] { book => JsString(book.string) }
+}
