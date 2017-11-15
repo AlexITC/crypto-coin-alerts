52f25612d91c49c3612b0ded2ef259005ee0502b
Alexis Hernandez <alexis22229@gmail.com>
2017-11-15 02:14:09 -0600

    alerts-server: Add experimental version of AlertsTask
    AlertsTask checks the markets for the current tickers
    and triggers email alerts based on this information.

--- alerts-server/app/com/alexitc/coinalerts/modules/AlertTaskModule.scala
+++ alerts-server/app/com/alexitc/coinalerts/modules/AlertTaskModule.scala
@@ -1,13 +1,15 @@
 package com.alexitc.coinalerts.modules
 
-import javax.inject.Inject
+import javax.inject.{Inject, Singleton}
 
 import akka.actor.ActorSystem
 import com.alexitc.coinalerts.config.AlertTaskConfig
+import com.alexitc.coinalerts.tasks.AlertsTask
 import org.slf4j.LoggerFactory
 import play.api.inject.{SimpleModule, _}
 
 import scala.concurrent.ExecutionContext
+import scala.util.control.NonFatal
 
 class AlertTaskModule
     extends SimpleModule(bind[AlertTaskRunner].toSelf.eagerly())
@@ -15,28 +17,36 @@ class AlertTaskModule
 /**
  * Runs the alert task frequently.
  */
+@Singleton
 class AlertTaskRunner @Inject() (
     actorSystem: ActorSystem,
-    config: AlertTaskConfig)(
+    config: AlertTaskConfig,
+    alertsTask: AlertsTask)(
     implicit executionContext: ExecutionContext) {
 
   private val logger = LoggerFactory.getLogger(this.getClass)
 
+  start()
+
+  // TODO: Add shutdown hook?
   def start() = {
     if (config.enabled) {
       logger.info("Starting alert task runner...")
 
-      actorSystem.scheduler.schedule(
+      val _ = actorSystem.scheduler.schedule(
         initialDelay = config.initialDelay,
-        interval = config.interval,
-      )(runTask)
+        interval = config.interval) { runTask() }
     } else {
       logger.info("Alert task is disabled")
     }
   }
 
-
-  def runTask = {
+  def runTask(): Unit = {
     logger.info("Running alert task...")
+    val _ = alertsTask.execute()
+        .recover {
+          case NonFatal(ex) =>
+            logger.error("Unexpected error while running TriggerAlertsTask", ex)
+        }
   }
 }
---
+++ alerts-server/app/com/alexitc/coinalerts/tasks/AlertsTask.scala
@@ -0,0 +1,82 @@
+package com.alexitc.coinalerts.tasks
+
+import javax.inject.Inject
+
+import com.alexitc.coinalerts.config.TaskExecutionContext
+import com.alexitc.coinalerts.data.async.{AlertFutureDataHandler, UserAsyncDAL}
+import com.alexitc.coinalerts.models._
+import com.alexitc.coinalerts.services.EmailServiceTrait
+import org.scalactic.{Bad, Good}
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.Future
+
+class AlertsTask @Inject() (
+    bitsoAlertCollector: BitsoAlertCollector,
+    userAsyncDAL: UserAsyncDAL,
+    alertDataHandler: AlertFutureDataHandler,
+    emailServiceTrait: EmailServiceTrait)(
+    implicit ec: TaskExecutionContext) {
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  def execute(): Future[Unit] = {
+    bitsoAlertCollector
+        .collect()
+        .map(groupByUser)
+        .flatMap { userAlerts =>
+          userAlerts.foreach {
+            case (userId, eventList) => triggerAlerts(userId, eventList)
+          }
+
+          Future.unit
+        }
+  }
+
+  private def groupByUser(eventList: List[AlertEvent]): Map[UserId, List[AlertEvent]] = {
+    eventList.groupBy(_.alert.userId)
+  }
+
+  private def triggerAlerts(userId: UserId, eventList: List[AlertEvent]): Future[Unit] = {
+    val text = groupByMarket(eventList)
+        .map {
+          case (market, marketEvents) =>
+            val marketLines = marketEvents.map(createText).mkString("\n")
+            s"${market.string}:\n$marketLines"
+        }
+        .mkString("\n\n\n")
+
+    userAsyncDAL.getVerifiedUserById(userId).flatMap {
+      case Good(user) =>
+        // TODO: i18n
+        val header = "Your Crypto Coin Alerts"
+        emailServiceTrait.sendEmail(user.email, header, text).flatMap {
+          case Bad(errors) =>
+            logger.error(s"Error while sending alerts by email to user = [${userId.string}], errors = [$errors]")
+            Future.unit
+
+          case _ =>
+            eventList.foreach { event =>
+              alertDataHandler.markAsTriggered(event.alert.id)
+            }
+
+            Future.unit
+        }
+      case Bad(errors) =>
+        logger.error(s"Error while retrieving user = [${userId.string}] for sending alerts, errors = [$errors]")
+        Future.unit
+    }
+  }
+
+  private def groupByMarket(eventList: List[AlertEvent]): Map[Market, List[AlertEvent]] = {
+    eventList.groupBy(_.alert.market)
+  }
+
+  // TODO: i18n
+  private def createText(event: AlertEvent): String = {
+    val alert = event.alert
+    val action = if (alert.isGreaterThan) "increased" else "decreased"
+    // TODO: Compute % difference for base_price alerts
+    s"${alert.book.string} has $action to ${event.currentPrice}"
+  }
+}
