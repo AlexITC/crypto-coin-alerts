077b8b490817dfb511543c1a429f08f8ffd1325a
Alexis Hernandez <alexis22229@gmail.com>
2017-11-17 01:02:41 -0600

    alerts-server: Add EmailMessagesProvider

---
+++ alerts-server/app/com/alexitc/coinalerts/services/EmailMessagesProvider.scala
@@ -0,0 +1,22 @@
+package com.alexitc.coinalerts.services
+
+import javax.inject.Inject
+
+import com.alexitc.coinalerts.models.UserVerificationToken
+import play.api.i18n.{Lang, MessagesApi}
+
+class EmailMessagesProvider @Inject() (messagesApi: MessagesApi) {
+
+  def verifyEmailSubject(implicit lang: Lang): EmailSubject = {
+    val string = messagesApi("email.verificationToken.subject")
+    new EmailSubject(string)
+  }
+
+  def verifyEmailText(token: UserVerificationToken)(implicit lang: Lang): EmailText = {
+    val string = messagesApi("email.verificationToken.text", token.string)
+    new EmailText(string)
+  }
+}
+
+class EmailSubject(val string: String) extends AnyVal
+class EmailText(val string: String) extends AnyVal
