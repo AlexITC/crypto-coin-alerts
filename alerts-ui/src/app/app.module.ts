e93b70260e93d52b62860da90d8d7db6cf4e3863
Alexis Hernandez <alexis22229@gmail.com>
2017-11-19 22:01:37 -0600

    alerts-ui: Add bootstrap integration

--- alerts-ui/src/app/app.module.ts
+++ alerts-ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AlertModule, BsDropdownModule, TooltipModule, ModalModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    AlertModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     BrowserModule
   ],
   providers: [],
