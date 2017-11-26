2beb3c15d66f1f4f12233c29e1c13cb8439de44d
Alexis Hernandez <alexis22229@gmail.com>
2017-11-25 22:05:20 -0600

    alerts-server: Add play query string binder for PaginatedQuery

--- alerts-server/app/com/alexitc/coinalerts/commons/PlayBinders.scala
+++ alerts-server/app/com/alexitc/coinalerts/commons/PlayBinders.scala
@@ -1,7 +1,8 @@
 package com.alexitc.coinalerts.commons
 
+import com.alexitc.coinalerts.core.{Limit, Offset, PaginatedQuery}
 import com.alexitc.coinalerts.models.UserVerificationToken
-import play.api.mvc.PathBindable
+import play.api.mvc.{PathBindable, QueryStringBindable}
 
 object PlayBinders {
 
@@ -14,6 +15,27 @@ object PlayBinders {
 
     override def unbind(key: String, token: UserVerificationToken): String = {
       token.string
+    }
+  }
+
+  private val DefaultOffset = 0
+  private val DefaultLimit = 20
+
+  implicit def paginatedQueryBinder(implicit binder: QueryStringBindable[Int]) = new QueryStringBindable[PaginatedQuery] {
+    override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, PaginatedQuery]] = {
+      val result = for {
+        offsetInt <- binder.bind("offset", params).getOrElse(Right(DefaultOffset))
+        limitInt <- binder.bind("limit", params).getOrElse(Right(DefaultLimit))
+      } yield PaginatedQuery(Offset(offsetInt), Limit(limitInt))
+
+      Some(result)
+    }
+
+    override def unbind(key: String, value: PaginatedQuery): String = {
+      val offsetParam = binder.unbind("offset", value.offset.int)
+      val limitParam = binder.unbind("limit", value.limit.int)
+
+      List(offsetParam, limitParam).mkString("&")
     }
   }
 }
