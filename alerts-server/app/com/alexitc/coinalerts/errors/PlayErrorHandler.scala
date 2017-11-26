b3ef2798ac73cba347bb205419c6d7b80ab51fed
Alexis Hernandez <alexis22229@gmail.com>
2017-11-25 22:06:28 -0600

    alerts-server: Add PlayErrorHandler

---
+++ alerts-server/app/com/alexitc/coinalerts/errors/PlayErrorHandler.scala
@@ -0,0 +1,41 @@
+package com.alexitc.coinalerts.errors
+
+import javax.inject.{Inject, Singleton}
+
+import com.alexitc.coinalerts.core.ErrorId
+import org.slf4j.LoggerFactory
+import play.api.http.HttpErrorHandler
+import play.api.libs.json.Json
+import play.api.mvc.Results._
+import play.api.mvc._
+
+import scala.concurrent._
+
+@Singleton
+class PlayErrorHandler @Inject() (errorRenderer: JsonErrorRenderer) extends HttpErrorHandler {
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
+    val publicError = errorRenderer.toPublicError(message)
+    val error = errorRenderer.renderPublicError(publicError)
+    val json = Json.obj(
+      "errors" -> Json.arr(error)
+    )
+
+    val result = Status(statusCode)(json)
+    Future.successful(result)
+  }
+
+  def onServerError(request: RequestHeader, exception: Throwable) = {
+    val errorId = ErrorId.create
+    val error = errorRenderer.renderPrivateError(errorId)
+    val json = Json.obj(
+      "errors" -> Json.arr(error)
+    )
+
+    logger.error(s"Server error, errorId = [${errorId.string}]", exception)
+    val result = InternalServerError(json)
+    Future.successful(result)
+  }
+}
