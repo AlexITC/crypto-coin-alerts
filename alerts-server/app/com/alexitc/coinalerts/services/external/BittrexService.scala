54538e81cf5e996db2310614ea285e13136a3859
Alexis Hernandez <alexis22229@gmail.com>
2017-12-04 01:27:17 -0600

    alerts-server: Rename BittrexClient to BittrexService

--- alerts-server/app/com/alexitc/coinalerts/services/external/BittrexService.scala
+++ alerts-server/app/com/alexitc/coinalerts/services/external/BittrexService.scala
@@ -1,40 +1,33 @@
-package com.alexitc.coinalerts.tasks.clients
+package com.alexitc.coinalerts.services.external
 
 import javax.inject.Inject
 
-import com.alexitc.coinalerts.config.TaskExecutionContext
 import com.alexitc.coinalerts.models.Book
 import com.alexitc.coinalerts.tasks.models.Ticker
 import org.slf4j.LoggerFactory
 import play.api.libs.functional.syntax._
 import play.api.libs.json.{JsPath, Reads}
-import play.api.libs.ws.WSClient
+import play.api.libs.ws.{WSClient, WSResponse}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class BittrexClient @Inject() (
-    ws: WSClient)(
-    implicit ec: TaskExecutionContext) {
+class BittrexService @Inject() (ws: WSClient)(implicit ec: ExecutionContext) {
 
   private val logger = LoggerFactory.getLogger(this.getClass)
 
+  private val BaseURL = "https://bittrex.com/api/v1.1/public"
+
   def getTickerList(): Future[List[Ticker]] = {
-    val url = "https://bittrex.com/api/v1.1/public/getmarketsummaries"
+    val url = s"$BaseURL/getmarketsummaries"
     ws.url(url)
         .get()
         .map { response =>
           Option(response)
-              .filter(_.status == 200)
-              .map(_.json)
+              .flatMap(toJson)
               .flatMap { json =>
-                (json \ "success")
-                    .asOpt[Boolean]
-                    .filter(identity)
-                    .flatMap { _ =>
-                      (json \ "result")
-                          .asOpt[List[Option[Ticker]]]
-                          .map(_.flatten)
-                    }
+                (json \ "result")
+                    .asOpt[List[Option[Ticker]]]
+                    .map(_.flatten)
               }
               .getOrElse {
                 logger.warn(s"Unexpected response from BITTREX, status = [${response.status}]")
@@ -42,6 +35,18 @@ class BittrexClient @Inject() (
               }
         }
 
+  }
+
+  private def toJson(response: WSResponse) = {
+    if (response.status != 200) {
+      None
+    } else {
+      val json = response.json
+      (json \ "success")
+          .asOpt[Boolean]
+          .filter(identity)
+          .map(_ => json)
+    }
   }
 
   implicit val tickerReads: Reads[Option[Ticker]] = {
--- alerts-server/app/com/alexitc/coinalerts/tasks/collectors/BittrexTickerCollector.scala
+++ alerts-server/app/com/alexitc/coinalerts/tasks/collectors/BittrexTickerCollector.scala
@@ -3,16 +3,16 @@ package com.alexitc.coinalerts.tasks.collectors
 import javax.inject.Inject
 
 import com.alexitc.coinalerts.models._
-import com.alexitc.coinalerts.tasks.clients.BittrexClient
+import com.alexitc.coinalerts.services.external.BittrexService
 import com.alexitc.coinalerts.tasks.models.Ticker
 
 import scala.concurrent.Future
 
-class BittrexTickerCollector @Inject() (bittrexClient: BittrexClient) extends TickerCollector {
+class BittrexTickerCollector @Inject() (bittrexService: BittrexService) extends TickerCollector {
 
   override val market: Market = Market.BITTREX
 
   override def getTickerList: Future[List[Ticker]] = {
-    bittrexClient.getTickerList()
+    bittrexService.getTickerList()
   }
 }
