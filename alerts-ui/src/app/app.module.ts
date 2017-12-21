172de23ac7a0c42bb546f55a3173490cdd9b5142
Alexis Hernandez <alexis22229@gmail.com>
2017-12-21 00:15:27 -0600

    alerts-ui: Integrate ngx-translate library

--- alerts-ui/src/app/app.module.ts
+++ alerts-ui/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AlertModule, BsDropdownModule, TooltipModule, ModalModule } from 'ngx-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
@@ -23,7 +24,8 @@ import { UsersService } from './users.service';
     ModalModule.forRoot(),
     BrowserModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot()
   ],
   providers: [UsersService],
   bootstrap: [AppComponent]
