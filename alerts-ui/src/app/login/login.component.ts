82c3f10752e48997fcff657a0b204718bccf8fa8
Alexis Hernandez <alexis22229@gmail.com>
2017-12-24 17:00:13 -0600

    alerts-ui: Use ErrorService on LoginComponent

--- alerts-ui/src/app/login/login.component.ts
+++ alerts-ui/src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/fo
 import { AuthService } from '../auth.service';
 import { UsersService } from '../users.service';
 import { AuthorizationToken } from '../authorization-token';
+import { ErrorService } from '../error.service';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +19,8 @@ export class LoginComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private usersService: UsersService,
-    private authService: AuthService) {
+    private authService: AuthService,
+    public errorService: ErrorService) {
 
     this.createForm();
   }
@@ -64,55 +66,8 @@ export class LoginComponent implements OnInit {
       if (element.type === 'field-validation-error') {
         const fieldName = element.field;
         const message = element.message;
-        this.setFieldError(fieldName, message);
+        this.errorService.setFieldError(this.form, fieldName, message);
       }
     });
-  }
-
-  // TODO: most logic here is repeated
-  protected setFieldError(fieldName: string, message: string) {
-    const control = this.findFieldControl(fieldName);
-    // TODO: find a better way to set the error message
-    const errors = { [message]: true };
-    control.setErrors(errors);
-  }
-
-  hasWrongValue(fieldName: string): boolean {
-    return this.getFieldErrors(fieldName).length > 0;
-  }
-
-  hasCorrectValue(fieldName: string): boolean {
-    const control = this.findFieldControl(fieldName);
-    // field found && user changed it && it doesn't hold a wrong value
-    const isCorrect = control && !control.pristine && !this.hasWrongValue(fieldName);
-
-    return isCorrect;
-  }
-
-  // right now we are rendering one error only for a field
-  getFieldError(fieldName: string): string {
-    return this.getFieldErrors(fieldName)[0];
-  }
-
-  getFieldErrors(fieldName: string): string[] {
-    const control = this.findFieldControl(fieldName);
-    if (control && (control.touched || this.submitted) && control.errors) {
-      return this.getErrors(control);
-    } else {
-      return [];
-    }
-  }
-
-  protected getErrors(control: AbstractControl): string[] {
-    return Object.keys(control.errors)
-      .filter((error: any) => control.errors[error])
-      .map((error: any) => {
-        const params = control.errors[error];
-        return error;
-      });
-  }
-
-  protected findFieldControl(fieldName: string): AbstractControl {
-    return this.form.get(fieldName);
   }
 }
