cebb12bb2c6ef9383222ee7d4f80e3b072af7942
Alexis Hernandez <alexis22229@gmail.com>
2018-01-02 13:25:01 -0600

    alerts-server: Add LoggingFilter

---
+++ alerts-server/app/com/alexitc/coinalerts/filters/LoggingFilter.scala
@@ -0,0 +1,33 @@
+package com.alexitc.coinalerts.filters
+
+import javax.inject.Inject
+
+import akka.stream.Materializer
+import org.slf4j.LoggerFactory
+import play.api.mvc.{Filter, RequestHeader, Result}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class LoggingFilter @Inject() (
+    implicit val mat: Materializer,
+    ec: ExecutionContext)
+    extends Filter {
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  def apply(nextFilter: RequestHeader => Future[Result])
+      (requestHeader: RequestHeader): Future[Result] = {
+
+    val startTime = System.currentTimeMillis
+
+    nextFilter(requestHeader).map { result =>
+
+      val endTime = System.currentTimeMillis
+      val requestTime = endTime - startTime
+
+      logger.info(s"${requestHeader.method} ${requestHeader.uri} took $requestTime ms and returned ${result.header.status}")
+
+      result.withHeaders("Request-Time" -> requestTime.toString)
+    }
+  }
+}
