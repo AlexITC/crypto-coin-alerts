a715e5ea96d64de3081835a7696333d54dae0e51
Alexis Hernandez <alexis22229@gmail.com>
2018-01-12 00:31:33 -0600

    alerts-ui: Add caching on ExchangeCurrencyService

--- alerts-ui/src/app/exchange-currency.service.ts
+++ alerts-ui/src/app/exchange-currency.service.ts
@@ -2,29 +2,89 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/observable/of';
+
 import { environment } from '../environments/environment';
 import { ExchangeCurrency } from './exchange-currency';
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
 
 @Injectable()
 export class ExchangeCurrencyService {
 
   private baseUrl = environment.api.url + '/exchanges';
 
+  // these data doesn't change frequently
+  private exchangeMarketsCache = {};
+  private marketCurrenciesCache = {};
+  private exchangeCurrencyCache = {};
+
   constructor(private http: HttpClient) { }
 
-  getMarkets(exchange: string): Observable<string[]> {
-    const url = `${this.baseUrl}/${exchange}/markets`;
+  /* caching */
+  private hasExchangeMarkets(exchange: string): boolean {
+    return this.exchangeMarketsCache[exchange] != null;
+  }
 
-    return this.http.get<string[]>(url, httpOptions);
+  private cacheExchangeMarkets(exchange, markets: string[]) {
+    this.exchangeMarketsCache[exchange] = markets;
+  }
+
+  private hasMarketCurrencies(exchange: string, market: string): boolean {
+    return this.marketCurrenciesCache[exchange] != null &&
+           this.marketCurrenciesCache[exchange][market] != null;
+  }
+
+  private cacheMarketCurrencies(exchange: string, market: string, currencies: ExchangeCurrency[]) {
+    if (this.marketCurrenciesCache[exchange] == null) {
+      this.marketCurrenciesCache[exchange] = {};
+    }
+
+    this.marketCurrenciesCache[exchange][market] = currencies;
+  }
+
+  private hasExchangeCurrency(id: number): boolean {
+    return this.exchangeCurrencyCache[id] != null;
+  }
+
+  private cacheExchangeCurrency(id: number, exchangeCurrency: ExchangeCurrency) {
+    this.exchangeCurrencyCache[id] = exchangeCurrency;
+  }
+
+  /* actual methods */
+  getMarkets(exchange: string): Observable<string[]> {
+    if (exchange == null || exchange === '') {
+      return Observable.of([]);
+    }
+
+    if (this.hasExchangeMarkets(exchange)) {
+      return Observable.of(this.exchangeMarketsCache[exchange]);
+    }
+
+    const url = `${this.baseUrl}/${exchange}/markets`;
+    return this.http.get<string[]>(url)
+             .do(markets => this.cacheExchangeMarkets(exchange, markets));
   }
 
   getCurrencies(exchange: string, market: string): Observable<ExchangeCurrency[]> {
-    const url = `${this.baseUrl}/${exchange}/markets/${market}`;
+    if (market == null || market === '') {
+      return Observable.of([]);
+    }
 
-    return this.http.get<ExchangeCurrency[]>(url, httpOptions);
+    if (this.hasMarketCurrencies(exchange, market)) {
+      return Observable.of(this.marketCurrenciesCache[exchange][market]);
+    }
+
+    const url = `${this.baseUrl}/${exchange}/markets/${market}/currencies`;
+    return this.http.get<ExchangeCurrency[]>(url)
+             .do(currencies => this.cacheMarketCurrencies(exchange, market, currencies));
+  }
+
+  getExchangeCurrency(id: number): Observable<ExchangeCurrency> {
+    if (this.hasExchangeCurrency(id)) {
+      return Observable.of(this.exchangeCurrencyCache[id]);
+    }
+
+    const url = `${this.baseUrl}/currencies/${id}`;
+    return this.http.get<ExchangeCurrency>(url)
+             .do(c => this.cacheExchangeCurrency(id, c));
   }
 }
