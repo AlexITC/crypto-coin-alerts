d4879bc761e7e39cd36325b9588b98fd713af8f9
Alexis Hernandez <alexis22229@gmail.com>
2018-01-17 21:06:47 -0600

    alerts-server: Update ExchangeCurrencySeederTask to be a ShutdownableTaskRunner

--- alerts-server/app/com/alexitc/coinalerts/modules/ExchangeCurrencySeederTaskModule.scala
+++ alerts-server/app/com/alexitc/coinalerts/modules/ExchangeCurrencySeederTaskModule.scala
@@ -4,45 +4,30 @@ import javax.inject.{Inject, Singleton}
 
 import akka.actor.ActorSystem
 import com.alexitc.coinalerts.config.ExchangeCurrencySeederTaskConfig
-import com.alexitc.coinalerts.tasks.ExchangeCurrencySeederTask
-import org.slf4j.LoggerFactory
+import com.alexitc.coinalerts.core.ShutdownHandler
+import com.alexitc.coinalerts.tasks.{ExchangeCurrencySeederTask, ShutdownableTaskRunner}
 import play.api.inject.{SimpleModule, bind}
 
-import scala.concurrent.ExecutionContext
-import scala.util.control.NonFatal
+import scala.concurrent.duration.FiniteDuration
 
 class ExchangeCurrencySeederTaskModule
     extends SimpleModule(bind[ExchangeCurrencySeederTaskRunner].toSelf.eagerly())
 
-/**
- * Runs the alert task frequently.
- */
 @Singleton
 class ExchangeCurrencySeederTaskRunner @Inject() (
-    actorSystem: ActorSystem,
+    protected val shutdownHandler: ShutdownHandler,
+    protected val actorSystem: ActorSystem,
     config: ExchangeCurrencySeederTaskConfig,
-    exchangeCurrencySeederTask: ExchangeCurrencySeederTask)(
-    implicit executionContext: ExecutionContext) {
+    exchangeCurrencySeederTask: ExchangeCurrencySeederTask)
+    extends ShutdownableTaskRunner {
 
-  private val logger = LoggerFactory.getLogger(this.getClass)
+  override protected def initialDelay: FiniteDuration = config.initialDelay
 
-  start()
+  override protected def interval: FiniteDuration = config.interval
 
-  // TODO: Add shutdown hook?
-  def start() = {
-    logger.info("Starting currency seeder runner...")
-
-    val _ = actorSystem.scheduler.schedule(
-      initialDelay = config.initialDelay,
-      interval = config.interval) { runTask() }
+  override protected def run() = {
+    exchangeCurrencySeederTask.execute()
   }
 
-  def runTask(): Unit = {
-    logger.info("Running currency seeder task...")
-    val _ = exchangeCurrencySeederTask.execute()
-        .recover {
-          case NonFatal(ex) =>
-            logger.error("Unexpected error while running currency seeder task", ex)
-        }
-  }
+  register()
 }
