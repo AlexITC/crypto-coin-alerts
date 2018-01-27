96716b65fdd47158163c956ffa9bc5994bf2ed0a
Alexis Hernandez <alexis22229@gmail.com>
2018-01-27 16:56:31 -0600

    alerts-server: Add BinanceService

---
+++ alerts-server/app/com/alexitc/coinalerts/services/external/BinanceService.scala
@@ -0,0 +1,93 @@
+package com.alexitc.coinalerts.services.external
+
+import javax.inject.Inject
+
+import com.alexitc.coinalerts.config.ExternalServiceExecutionContext
+import com.alexitc.coinalerts.models.{Book, Currency, Market}
+import com.alexitc.coinalerts.tasks.models.Ticker
+import org.slf4j.LoggerFactory
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsPath, Reads}
+import play.api.libs.ws.{WSClient, WSResponse}
+
+import scala.concurrent.Future
+
+class BinanceService @Inject() (
+    ws: WSClient)(
+    implicit ec: ExternalServiceExecutionContext)
+    extends ExchangeService {
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  private val BaseURL = "https://api.binance.com/api"
+
+  /**
+   * Binance gives us a book without a separator between the market and the currency,
+   * for example: DASHBTC means BTC as market and DASH as currency.
+   *
+   * Then, we need to know the supported markets in advance to perform this parsing.
+   */
+  private val KnownMarkets = "BTC ETH USDT BNB".split(" ").map(Market.apply).toList
+
+  override def availableBooks(): Future[List[Book]] = {
+    getTickerList()
+        .map { tickerList =>
+          tickerList.map(_.book)
+        }
+  }
+
+  override def getTickerList(): Future[List[Ticker]] = {
+    val url = s"$BaseURL/v1/ticker/allPrices"
+
+    ws.url(url)
+        .get()
+        .map { response =>
+          Option(response)
+              .flatMap(toJson)
+              .flatMap { json =>
+                json
+                    .asOpt[List[Option[Ticker]]]
+                    .map(_.flatten)
+              }
+              .getOrElse {
+                logger.warn(s"Unexpected response from BINANCE, status = [${response.status}]")
+                List.empty
+              }
+        }
+
+  }
+
+  private def toJson(response: WSResponse) = {
+    if (response.status != 200) {
+      None
+    } else {
+      val json = response.json
+      Some(json)
+    }
+  }
+
+  private implicit val tickerReads: Reads[Option[Ticker]] = {
+    val builder = (JsPath \ "symbol").read[String].map(_.toUpperCase).map(createBook) and
+        (JsPath \ "price").read[BigDecimal]
+
+    builder.apply { (bookMaybe, currentPrice) =>
+      bookMaybe.map { book =>
+        Ticker(book, currentPrice)
+      }
+    }
+  }
+
+  private def createBook(string: String): Option[Book] = {
+    KnownMarkets
+        .find { market => string.endsWith(market.string) }
+        .map { market =>
+          val currencyStr = string.substring(0, string.length - market.string.length)
+          val currency = Currency(currencyStr)
+          Book(market, currency)
+        }
+        .orElse {
+          logger.warn(s"Unable to create book from string = [$string]")
+          None
+        }
+  }
+}
---
+++ alerts-server/test/com/alexitc/coinalerts/services/external/BinanceServiceSpec.scala
@@ -0,0 +1,90 @@
+package com.alexitc.coinalerts.services.external
+
+import com.alexitc.coinalerts.commons.ExecutionContexts
+import com.alexitc.coinalerts.models.Book
+import com.alexitc.coinalerts.tasks.models.Ticker
+import org.mockito.Matchers.anyString
+import org.mockito.Mockito.when
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{MustMatchers, WordSpec}
+import play.api.libs.json.Json
+import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
+
+import scala.concurrent.Future
+
+class BinanceServiceSpec extends WordSpec with MustMatchers with ScalaFutures with MockitoSugar {
+
+  val ws = mock[WSClient]
+  val ec = ExecutionContexts.externalServiceEC
+  val service = new BinanceService(ws)(ec)
+
+  val responseBody =
+    """
+      |[
+      |  {
+      |    "symbol": "DASHBTC",
+      |    "price": "0.06732200"
+      |  },
+      |  {
+      |    "symbol": "DASHETH",
+      |    "price": "0.69579000"
+      |  },
+      |  {
+      |    "symbol": "LTCUSDT",
+      |    "price": "181.63000000"
+      |  },
+      |  {
+      |    "symbol": "LTCBNB",
+      |    "price": "13.62000000"
+      |  }
+      |]
+    """.stripMargin
+
+  "availableBooks" should {
+    "retrieve available books" in {
+      val expectedBooks = "BTC_DASH ETH_DASH USDT_LTC BNB_LTC".split(" ").map(Book.fromString).map(_.get).toList
+
+      val request = mock[WSRequest]
+      val response = mock[WSResponse]
+      val json = Json.parse(responseBody)
+
+      when(ws.url(anyString)).thenReturn(request)
+      when(response.status).thenReturn(200)
+      when(response.json).thenReturn(json)
+      when(request.get()).thenReturn(Future.successful(response))
+
+      whenReady(service.availableBooks()) { books =>
+        books.size mustEqual expectedBooks.size
+
+        books.sortBy(_.string) mustEqual expectedBooks.sortBy(_.string)
+      }
+    }
+  }
+
+  "getTickerList" should {
+    "retrieve the ticket" in {
+      val expectedTickers = List(
+        "BTC_DASH" -> BigDecimal("0.06732200"),
+        "ETH_DASH" -> BigDecimal("0.69579000"),
+        "USDT_LTC" -> BigDecimal("181.63000000"),
+        "BNB_LTC" -> BigDecimal("13.62000000")
+      ).map { case (string, price) => Ticker(Book.fromString(string).get, price) }
+
+      val request = mock[WSRequest]
+      val response = mock[WSResponse]
+      val json = Json.parse(responseBody)
+
+      when(ws.url(anyString)).thenReturn(request)
+      when(response.status).thenReturn(200)
+      when(response.json).thenReturn(json)
+      when(request.get()).thenReturn(Future.successful(response))
+
+      whenReady(service.getTickerList()) { tickerList =>
+        tickerList.size mustEqual expectedTickers.size
+
+        tickerList.sortBy(_.book.string) mustEqual expectedTickers.sortBy(_.book.string)
+      }
+    }
+  }
+}
